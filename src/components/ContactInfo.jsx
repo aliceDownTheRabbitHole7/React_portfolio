968b4e014d660531825d9283919ce6c81b87e97d
Aleksander White <zander1116@aol.com>
2023-10-11 20:57:48 -0700

    first commit

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,14 @@
+function ContactInfo() {
+    return (
+        <>
+            <div className="d-flex justify-content-between p-3 m-3" id='contact-head'>
+                <h2>Aleksander White</h2>
+                <h3>(779)255-0508</h3>
+                <h3>aleksanderw1116@gmail.com</h3>
+                <h3>Kirkland, IL</h3>
+            </div>
+        </>
+    )
+}
+
+export default ContactInfo;
