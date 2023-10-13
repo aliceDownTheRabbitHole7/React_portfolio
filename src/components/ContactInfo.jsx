ee4d25fbec277506934a3bba8981e3b1f55505a4
Aleksander White <zander1116@aol.com>
2023-10-13 16:54:04 -0700

    Minor changes to the contact info

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -2,8 +2,8 @@ function ContactInfo() {
   return (
     <>
       <div id="contact-head">
-        <h2 className="contact-info" id='name'><b>Aleksander White</b></h2>
-        <h6 className="contact-info"><i class="fa-solid fa-phone"></i> (779)255-0508</h6>
+        <h4 className="contact-info" id='name'><b>Aleksander White</b></h4>
+        <h6 className="contact-info"><i class="fa-solid fa-phone"></i> (779)255-0508  </h6>
         <h6 className="contact-info"><i class="fa-solid fa-envelope"></i> aleksanderw1116@gmail.com</h6>
         <h6 className="contact-info"><i class="fa-solid fa-location-dot"></i> Kirkland, IL</h6>
       </div>
