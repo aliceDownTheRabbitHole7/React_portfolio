dbcbf9f7aeb6a12b22ad0b6de27de027c740258c
Aleksander White <zander1116@aol.com>
2023-10-20 19:45:34 -0700

    switched the Stan Lee project for the Santa website

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,25 @@
 import projectsArray from "./ProjectsArray";
 
 function Projects() {
-  const projectsList = projectsArray.map((project) => (
-    <a href={project.link} key={project.ref} className="project" target='_blank'>
-      <img src={project.icon} key={project.id} />
-      <h3 className='project-head'>{project.head}</h3>
-    </a>
+
+  const projectsList = projectsArray.map((project, i) => (
+    <>
+      <a href={project.link} key={i} className="project" target='_blank'>
+        <img src={project.icon} />
+        <h3 className='project-head' key={project.ref}>{project.head}</h3>
+      </a>
+    </>
   ));
 
   return (
     <>
-      <h1 id="projects-head">Projects</h1>
+      <h1 id="projects-head" key='projects-head'>Projects</h1>
       <div className='projects-container'>
         {projectsList}
       </div>
     </>
   );
 }
+
 
 export default Projects;
--- src/components/Projects/ProjectsArray.jsx
+++ src/components/Projects/ProjectsArray.jsx
@@ -1,13 +1,13 @@
 import moleIcon from "../icons/icon-mole.png";
 import quoteIcon from "../icons/icon-quote.png";
-import ironmanIcon from "../icons/icon-ironman.png";
+import santaIcon from "../../assets/icon-santa.png";
 import markdownIcon from "../icons/icon-markdown.png";
 import jsCalculatorIcon from "../icons/icon-calculator.png";
 import blackJackIcon from "../icons/icon-blackjack.png";
 
 const whackAMole = "https://alicedowntherabbithole7.github.io/Whack-a-mole/";
 const quoteMachine = "https://alicedowntherabbithole7.github.io/quote_machine/";
-const stanLeeTribute = "https://alicedowntherabbithole7.github.io/tribute_page/";
+const santaPage = "https://comforting-unicorn-0ffafc.netlify.app/";
 const markdownPreviewer = "https://alicedowntherabbithole7.github.io/markdown_previewer/";
 const jsCalculator = "https://alicedowntherabbithole7.github.io/JS_Calculator/";
 const blackJack = "https://alicedowntherabbithole7.github.io/Black_jack/";
@@ -16,43 +16,31 @@ const projectsArray = [
   {
     icon: moleIcon,
     link: whackAMole,
-    id: "whack-a-mole",
-    ref: "whackAMole",
     head: "Whack-A-Mole!",
   },
   {
     icon: quoteIcon,
     link: quoteMachine,
-    id: "quote-generator",
-    ref: "quoteMachine",
     head: "Quote Generator",
   },
   {
-    icon: ironmanIcon,
-    link: stanLeeTribute,
-    id: "stan-lee",
-    ref: "stanLee",
-    head: "Stan Lee Tribute",
+    icon: santaIcon,
+    link: santaPage,
+    head: "Santa's Page",
   },
   {
     icon: markdownIcon,
     link: markdownPreviewer,
-    id: "markdown-previewer",
-    ref: "markdown",
     head: "Markdown Previewer",
   },
   {
     icon: jsCalculatorIcon,
     link: jsCalculator,
-    id: "js-calc",
-    ref: "jsCalc",
     head: "JS Calculator",
   },
   {
     icon: blackJackIcon,
     link: blackJack,
-    id: "black-jack",
-    ref: "blackJack",
     head: "Black Jack",
   },
 ];
